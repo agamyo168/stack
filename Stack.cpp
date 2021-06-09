9399b05f447d5cfb10d9cb6bc2c86f44d8993ded
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2021-06-09 07:29:29 +0200

    Working

--- Stack.cpp
+++ Stack.cpp
@@ -18,7 +18,6 @@ using namespace std;
     void pop()
     {  assert(size>0);
         size--;
-
     }
     char top()
     {  assert(size>0);
@@ -38,7 +37,7 @@ using namespace std;
 */
 int main()
 {
-    stack <char> s1;
+    stack <int> s1;
     int k;
     cin>>k;
     string x[MAX_STACK];
@@ -47,7 +46,8 @@ int main()
         cin>>x[i];
     }
     int flag;
-
+    int a = 0;
+    int b = 0;
     for(int i = 0; i < k ; ++i)
         {   flag = 1;
 
@@ -60,27 +60,31 @@ int main()
                         s1.push(x[i][j]);
                     }
                 //Close parentheses
+                if((x[i][j] == ')')||(x[i][j] == '}')||(x[i][j] =='>')||(x[i][j] == ']'))
+                    if(s1.empty())
+                        {cout<<"NO"<<endl;
+                        flag = 0;
+                            break;}
                 else if(!s1.empty()&&((x[i][j] == ')' && s1.top() == '(')||(x[i][j] == '}'&& s1.top() == '{')||(x[i][j] =='>'&& s1.top() =='<')||(x[i][j] == ']'&& s1.top() =='[')))
                         {
                         //cout<<x[i][j]<<" is poped from stack\n";
                         s1.pop();
                         }
                 else {
-                    cout<<"NO"<<endl;
-
                     flag = 0;
+                    cout<<"NO"<<endl;
                     break;
-
                 }
 
             }
-
             //Pushed = popped
             //Missing parentheses
-            if(!s1.empty()&& flag)
-                {cout<<"NO"<<endl;}
-            else if(s1.empty())
+            if(flag&&s1.empty())
+            {
                 cout<<"YES"<<endl;
+            }
+            else if(flag&&!s1.empty())
+                cout<<"NO"<<endl;
 
             while(!s1.empty())
                     s1.pop();
@@ -89,4 +93,3 @@ int main()
 
     return 0;
 }
-
